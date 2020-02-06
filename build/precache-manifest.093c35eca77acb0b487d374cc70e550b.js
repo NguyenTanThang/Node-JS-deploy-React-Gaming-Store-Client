7a5b4ba50f5e79ab544cb3d45eab5c152b555476
Victor <nguyentanthang9a4@gmail.com>
2020-02-06 09:42:01 +0700

    Version 1.0.0

---
+++ build/precache-manifest.093c35eca77acb0b487d374cc70e550b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ce3451dc123d0009951d5182635d3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b4a4f2630e6b33c014d",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ab449e9083839f25749e",
+    "url": "/static/css/main.384ab97f.chunk.css"
+  },
+  {
+    "revision": "9b4a4f2630e6b33c014d",
+    "url": "/static/js/2.6c9b85df.chunk.js"
+  },
+  {
+    "revision": "0117d6bfe178eaa3446727e0cf23470b",
+    "url": "/static/js/2.6c9b85df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab449e9083839f25749e",
+    "url": "/static/js/main.37b99c58.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
