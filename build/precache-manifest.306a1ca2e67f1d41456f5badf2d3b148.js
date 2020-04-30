c77ccae83d693a8cee741147c9ef1c8e872d7c34
Victor <nguyentanthang9a4@gmail.com>
2020-04-30 09:33:32 +0700

    Version 1.1.0 - Add in trailers and genres for all games

---
+++ build/precache-manifest.306a1ca2e67f1d41456f5badf2d3b148.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "992f0991f5434d5a90a600e254e90d03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b6877bd3ca6d17cb123",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "badc0bd31e3efeebe5d0",
+    "url": "/static/css/main.f3d01d9f.chunk.css"
+  },
+  {
+    "revision": "0b6877bd3ca6d17cb123",
+    "url": "/static/js/2.0e2550e6.chunk.js"
+  },
+  {
+    "revision": "0117d6bfe178eaa3446727e0cf23470b",
+    "url": "/static/js/2.0e2550e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "badc0bd31e3efeebe5d0",
+    "url": "/static/js/main.8930f1b7.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b2f7bfb4ad6ea113b1d35addaf09acf4.js"
+  "/precache-manifest.306a1ca2e67f1d41456f5badf2d3b148.js"
 );
 
 self.addEventListener('message', (event) => {
